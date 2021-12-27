41a696a40b25d10b4814ca3df28a3dd9ac489e3b
RehabRagab <rehab.abotalep@gmail.com>
2021-12-27 16:31:36 +0200

    Looping over arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -14,6 +14,10 @@ let notes = ['Note 1', 'Note 2', 'Note 3']
 
 notes[2] = 'This is now the new note 3'
 
+notes.forEach(function (item, index) {
+    console.log(`notes[${index}] = ${item}`)
+})
+
 
 console.log(notes.length)
 console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -10,12 +10,16 @@ const print = function (todo) {
     return `Todo : ${todo}`
 }
 
-console.log(`You have ${todos.length} todos`)
-
 todos.splice(2, 1)
 todos.push('New added todo')
 todos.shift()
 
+console.log(`You have ${todos.length} todos`)
+
+
+todos.forEach(function (item, index) {
+    console.log(`${index+1}. ${item}`)
+})
 // console.log(print(todos[0]))
 // console.log(print(todos[todos.length - 2]))
 
