8cf92e57e600e5f60b47f954d185caa368ed86dd
RehabRagab <rehab.abotalep@gmail.com>
2022-01-06 16:59:47 +0200

    Rendering filtered data

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,20 +1,51 @@
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
-// Query and remove
-const p = document.querySelector('p')
-p.remove()
+// // Query and remove
+// const p = document.querySelector('p')
+// p.remove()
 
-// Query and remove all 
-const ps = document.querySelectorAll('p')
-ps.forEach(function (p){
-    p.textContent = '******'
-    console.log(p.textContent)
-    //p.remove()
-})
+// // Query and remove all 
+// const ps = document.querySelectorAll('p')
+// ps.forEach(function (p){
+//     p.textContent = '******'
+//     console.log(p.textContent)
+//     //p.remove()
+// })
 
-// create new element
-const newParagraph = document.createElement('p')
-newParagraph.textContent = 'New paragraph from javascript'
-document.querySelector('body').appendChild(newParagraph)
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+
+// // create new element
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'New paragraph from javascript'
+// document.querySelector('body').appendChild(newParagraph)
+
+renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'button was clicked'
@@ -27,6 +58,7 @@ document.querySelector('#remove-all').addEventListener('click', function () {
 })
 
 document.querySelector('#search').addEventListener('input', function (e) {
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
 
