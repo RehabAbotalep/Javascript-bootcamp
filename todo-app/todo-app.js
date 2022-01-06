eedf9d001942a9862985567da4202de52650b9ad
RehabRagab <rehab.abotalep@gmail.com>
2022-01-06 17:14:40 +0200

    Todos filter challange

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -9,27 +9,41 @@ const todos = [{
     completed: true,
 }]
 
-const inCompletedTodos = todos.filter(function (todo) {
-    return !todo.completed
-})
+const filters = {
+    searchTxt: ''
+}
 
+const renderTods = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.title.toLowerCase().includes(filters.searchTxt.toLowerCase())
+    })
 
-const newParagraph = document.createElement('p')
-newParagraph.textContent = `You have ${inCompletedTodos.length} todos left`
-document.querySelector('body').appendChild(newParagraph)
+    const inCompletedTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
 
-todos.forEach(function (todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.title
-    document.querySelector('body').appendChild(p)
-})
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h1')
+    summary.textContent = `You have ${inCompletedTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.title
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTods(todos, filters)
 
 document.querySelector('#add-todo').addEventListener('click', function (e) {
     console.log('Adding new TODO')
 })
 
-document.querySelector('#new-todo').addEventListener('change', function (e) {
-    console.log(e.target.value)
+document.querySelector('#new-todo').addEventListener('input', function (e) {
+    filters.searchTxt = e.target.value
+    renderTods(todos, filters)
 })
 
 
